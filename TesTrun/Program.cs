00811b72b8b15ddb7e63551c51f5f3fc49257d67
David Božjak <bozjak@gmail.com>
2020-11-22 09:41:46 +0100

    Initial commit

---
+++ TesTrun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesTrun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
