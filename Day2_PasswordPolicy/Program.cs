9186941ba3f1bc680d8c020bf0109e6746b1e27e
David Božjak <bozjak@gmail.com>
2020-12-02 06:35:15 +0100

    Refactored to extract policy rule

--- Day2_PasswordPolicy/Program.cs
+++ Day2_PasswordPolicy/Program.cs
@@ -16,27 +16,40 @@ namespace Day2_PasswordPolicy
 
         static private void Part1()
         {
-            using var inputProvider = new InputProvider<PasswordPolicy>("Input.txt", ParseString);
+            using var inputProvider = new InputProvider<PasswordPolicy?>("Input.txt", ParseString);
 
-            var inputs = inputProvider.ToList();
+            var inputs = inputProvider.Where(w => w != null).Cast<PasswordPolicy>().ToList();
 
-            var count = inputs.Where(w => w.PasswordComplies(w.ExamplePassword)).Count();
+            var count = inputs.Where(w => PasswordComplies(w, w.ExamplePassword)).Count();
 
             Console.WriteLine($"{count} password comply with initial polciy");
+
+            bool PasswordComplies(PasswordPolicy policy, string input)
+            {
+                var count = input.Count(w => w == policy.RequiredChar);
+
+                return count >= policy.MinRepetition && count <= policy.MaxRepetition;
+            }
         }
 
         static private void Part2()
         {
-            using var inputProvider = new InputProvider<NewPasswordPolicy>("Input.txt", ParseString);
+            using var inputProvider = new InputProvider<PasswordPolicy?>("Input.txt", ParseString);
 
-            var inputs = inputProvider.ToList();
+            var inputs = inputProvider.Where(w => w != null).Cast<PasswordPolicy>().ToList();
 
-            var count = inputs.Where(w => w.PasswordComplies(w.ExamplePassword)).Count();
+            var count = inputs.Where(w => PasswordComplies(w, w.ExamplePassword)).Count();
 
             Console.WriteLine($"{count} password comply with the new polciy");
+
+            bool PasswordComplies(PasswordPolicy policy, string input)
+            {
+                return (input[policy.MinRepetition - 1] == policy.RequiredChar) ^
+                    (input[policy.MaxRepetition - 1] == policy.RequiredChar);
+            }
         }
 
-        static bool ParseString(string? value, out PasswordPolicy output)
+        static bool ParseString(string? value, out PasswordPolicy? output)
         {
             output = null;
 
@@ -45,24 +58,6 @@ namespace Day2_PasswordPolicy
             try
             {
                 output = new PasswordPolicy(value);
-
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        static bool ParseString(string? value, out NewPasswordPolicy output)
-        {
-            output = null;
-
-            if (string.IsNullOrWhiteSpace(value)) return false;
-
-            try
-            {
-                output = new NewPasswordPolicy(value);
 
                 return true;
             }
@@ -91,40 +86,6 @@ namespace Day2_PasswordPolicy
                 this.RequiredChar = charRegex.Matches(input).Select(w => w.Value[0]).First();
 
                 this.ExamplePassword = input.Substring(input.IndexOf(':') + 2);
-            }
-
-            public bool PasswordComplies(string input)
-            {
-                var count = input.Count(w => w == this.RequiredChar);
-
-                return count >= MinRepetition && count <= MaxRepetition;
-            }
-        }
-
-        class NewPasswordPolicy
-        {
-            public int MinRepetition { get; }
-            public int MaxRepetition { get; }
-            public char RequiredChar { get; }
-            public string ExamplePassword { get; }
-
-            public NewPasswordPolicy(string input)
-            {
-                var numRegex = new Regex(@"\d+");
-                var numMatches = numRegex.Matches(input).Select(w => int.Parse(w.Value)).ToArray();
-
-                this.MinRepetition = numMatches[0];
-                this.MaxRepetition = numMatches[1];
-
-                var charRegex = new Regex(@"[a-z]");
-                this.RequiredChar = charRegex.Matches(input).Select(w => w.Value[0]).First();
-
-                this.ExamplePassword = input.Substring(input.IndexOf(':') + 2);
-            }
-
-            public bool PasswordComplies(string input)
-            {
-                return (input[MinRepetition - 1] == this.RequiredChar) ^ (input[MaxRepetition - 1] == this.RequiredChar);
             }
         }
     }
