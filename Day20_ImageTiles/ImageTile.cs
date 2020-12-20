915bfa1ffac873f01f776b425621c72e142836ed
David Božjak <bozjak@gmail.com>
2020-12-20 21:04:48 +0100

    Still debugging, extracted ImageTile to file

---
+++ Day20_ImageTiles/ImageTile.cs
@@ -0,0 +1,210 @@
+﻿using SantasToolbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day20_ImageTiles
+{
+    partial class Program
+    {
+        class ImageTile
+        {
+            public int Id { get; private set;  }
+
+            public long TopLine => this.cachedTopLine.Value;
+            public string TopLineStr => this.GetEdgeString(Edge.Top);
+
+            public long BottomLine => this.cachedBottomLine.Value;
+            public string BottomLineStr => this.GetEdgeString(Edge.Bottom);
+
+            public long LeftLine => this.cachedLeftLine.Value;
+            public string LeftLineStr => this.GetEdgeString(Edge.Left);
+
+            public long RightLine => this.cachedRightLine.Value;
+            public string RightLineStr => this.GetEdgeString(Edge.Right);
+
+            private readonly List<string> originalRows = new List<string>();
+            private readonly List<string> rows = new List<string>();
+
+            private readonly Cached<long> cachedLeftLine;
+            private readonly Cached<long> cachedRightLine;
+            private readonly Cached<long> cachedBottomLine;
+            private readonly Cached<long> cachedTopLine;
+
+            private int rotation = 0;
+            private bool isFlippedHorizontally = false;
+            private bool isFlippedVerticallly = false;
+            private bool isFrozen = false;
+
+            public ImageTile()
+            {
+                this.cachedLeftLine = new Cached<long>(() => this.CalculateLine(Edge.Left));
+                this.cachedRightLine = new Cached<long>(() => this.CalculateLine(Edge.Right));
+                this.cachedBottomLine = new Cached<long>(() => this.CalculateLine(Edge.Bottom));
+                this.cachedTopLine = new Cached<long>(() => this.CalculateLine(Edge.Top));
+            }
+
+            public void AddRow(string row)
+            {
+                if (row.StartsWith("Tile"))
+                {
+                    this.SetId(row);
+                }
+                else
+                {
+                    this.rows.Add(row);
+                    this.ResetLines();
+                }
+            }
+
+            public void Freeze()
+            {
+                this.isFrozen = true;
+            }
+
+            public void UnFreeze()
+            {
+                this.isFrozen = false;
+            }
+
+            public void Rotate90()
+            {
+                if (this.isFrozen) throw new Exception();
+
+                this.rotation++;
+                this.ResetLines();
+            }
+
+            public void SetRotateSteps(int steps)
+            {
+                if (this.isFrozen) throw new Exception();
+
+                this.rotation = steps;
+                this.ResetLines();
+            }
+
+            public int Rotation => this.rotation;
+
+            public void Reset()
+            {
+                if (this.isFrozen) throw new Exception();
+
+                if (this.isFlippedHorizontally)
+                {
+                    this.FlipHorizontal();
+                }
+
+                if (this.isFlippedVerticallly)
+                {
+                    this.FlipVertical();
+                }
+
+                this.rotation = 0;
+                this.ResetLines();
+            }
+
+            public void SetIntoConfig(int config)
+            {
+                if (this.isFrozen) throw new Exception();
+                this.Reset();
+
+                if (config == 1)
+                {
+                    this.FlipVertical();
+                }
+                else if (config == 2)
+                {
+                    this.FlipHorizontal();
+                }
+                else if (config == 3)
+                {
+                    this.FlipVertical();
+                    this.FlipHorizontal();
+                }
+            }
+
+            public override string ToString() => this.Id.ToString();
+
+            private void FlipHorizontal()
+            {
+                if (this.isFrozen) throw new Exception();
+
+                var flippedRows = new List<string>();
+
+                for (int i = 0; i < this.rows.Count; i++)
+                {
+                    flippedRows.Add(new string(this.rows[i].Reverse().ToArray()));
+                }
+
+                this.rows.Clear();
+                this.rows.AddRange(flippedRows);
+
+                this.ResetLines();
+                this.isFlippedHorizontally = !this.isFlippedHorizontally;
+            }
+
+            private void FlipVertical()
+            {
+                this.rows.Reverse();
+                this.ResetLines();
+                this.isFlippedVerticallly = !this.isFlippedVerticallly;
+            }
+
+            private void SetId(string row)
+            {
+                var idString = row[(row.IndexOf(" ") + 1)..^1];
+                this.Id = int.Parse(idString);
+            }
+
+            enum Edge { Left = 1, Right = 2, Bottom = 3, Top = 4 }
+            private long CalculateLine(Edge edge)
+            {
+                string edgeStr = GetEdgeString(edge);
+                long value = 0;
+
+                for (int i = 0; i < edgeStr.Length; i++)
+                {
+                    if (edgeStr[i] == '#')
+                    {
+                        value += (long)Math.Pow(2, i);
+                    }
+                }
+
+                return value;
+            }
+
+            private string GetEdgeString(Edge edge)
+            {
+                var edgeAfterTotation = this.rotation + edge;
+                while ((int)edgeAfterTotation > 4) edgeAfterTotation -= 4;
+                while ((int)edgeAfterTotation <= 0) edgeAfterTotation += 4;
+
+                if (edgeAfterTotation == Edge.Top)
+                {
+                    return this.rows[0];
+                }
+                else if (edgeAfterTotation == Edge.Bottom)
+                {
+                    return this.rows.Last();
+                }
+                else if (edgeAfterTotation == Edge.Left)
+                {
+                    return new string(this.rows.Select(w => w[0]).ToArray());
+                }
+                else if (edgeAfterTotation == Edge.Right)
+                {
+                    return new string(this.rows.Select(w => w[^1]).ToArray());
+                }
+                else throw new Exception();
+            }
+
+            private void ResetLines()
+            {
+                this.cachedLeftLine.Reset();
+                this.cachedRightLine.Reset();
+                this.cachedBottomLine.Reset();
+                this.cachedTopLine.Reset();
+            }
+        }
+    }
+}
--- Day20_ImageTiles/Program.cs
+++ Day20_ImageTiles/Program.cs
@@ -5,10 +5,8 @@ using System.Linq;
 
 namespace Day20_ImageTiles
 {
-    class Program
+    partial class Program
     {
-        static int maxPlaced = 0;
-
         static void Main(string[] args)
         {
             var parser = new MultiLineParser<ImageTile>(() => new ImageTile(), (tile, row) => tile.AddRow(row));
@@ -32,7 +30,7 @@ namespace Day20_ImageTiles
 
             Console.WriteLine($"Part 1: {factorOfCornerTileIds}");
 
-            AssembleTilesToImage(tiles, cornerTiles, neighbourhood);
+            var workingGrid = AssembleTilesToImage(cornerTiles[0], tiles, neighbourhood);
         }
 
         private static Dictionary<ImageTile, IList<ImageTile>> FindNeighbouringTiles(IList<ImageTile> tiles)
@@ -86,74 +84,57 @@ namespace Day20_ImageTiles
             }
         }
 
-        private static Dictionary<(int x, int y), ImageTile>? AssembleTilesToImage(IList<ImageTile> tiles, IList<ImageTile> cornerTiles, Dictionary<ImageTile, IList<ImageTile>> neighbourhood)
+        private static Dictionary<(int x, int y), ImageTile>? AssembleTilesToImage(ImageTile imageTile, IList<ImageTile> tiles, Dictionary<ImageTile, IList<ImageTile>> neighbourhood)
         {
-            int sideLength = (int)Math.Sqrt(tiles.Count) - 1;
+            Dictionary<ImageTile, (int x, int y)> posInGrid = new Dictionary<ImageTile, (int x, int y)>();
+            Dictionary<(int x, int y), ImageTile> grid = new Dictionary<(int x, int y), ImageTile>();
+
             var topLeftPos = (0, 0);
-            //var topRightPos = (sideLength, 0);
-            //var bottomLeftPos = (0, sideLength);
-            //var bottomRightPos = (sideLength, sideLength);
+            var topLeft = imageTile;
+            var sideLength = (int)Math.Sqrt(tiles.Count);
+            posInGrid[topLeft] = topLeftPos;
+            grid[topLeftPos] = topLeft;
 
-            //var arrangements = cornerTiles.GetAllOrdersOfList().ToList();
-
-            // Temp get just the right ID to confirm it works
-            List<List<ImageTile>> arrangements = new List<List<ImageTile>>
+            for (int config = 0; config < 4; config++)
             {
-                new List<ImageTile>()
-                {
-                    tiles.First(w => w.Id == 1951),
-                    tiles.First(w => w.Id == 3079),
-                    tiles.First(w => w.Id == 2971),
-                    tiles.First(w => w.Id == 1171)
-                }
-            };
+                topLeft.UnFreeze();
+                topLeft.SetIntoConfig(config);
+                topLeft.Freeze();
 
-            for (int i = 0; i < arrangements.Count; i++)
-            {
-                Dictionary<ImageTile, (int x, int y)> posInGrid = new Dictionary<ImageTile, (int x, int y)>();
-                Dictionary<(int x, int y), ImageTile> grid = new Dictionary<(int x, int y), ImageTile>();
-
-                var alignedTiles = arrangements[i].ToList();
-                var tilesToAlign = tiles.Except(alignedTiles).ToList();
-
-                Console.WriteLine(string.Join(" ", alignedTiles.Select(w => w.Id)));
-
-                var topLeft = alignedTiles[0];
-                posInGrid[topLeft] = topLeftPos;
-                grid[topLeftPos] = topLeft;
-
-                for (int config = 0; config < 4; config++)
+                for (int rotation = 0; rotation < 4; rotation++)
                 {
                     topLeft.UnFreeze();
-                    topLeft.SetIntoConfig(config);
+                    topLeft.SetRotateSteps(rotation);
                     topLeft.Freeze();
 
-                    for (int rotation = 0; rotation < 4; rotation++)
-                    {
-                        topLeft.UnFreeze();
-                        topLeft.SetRotateSteps(rotation);
-                        topLeft.Freeze();
+                    var workingPosInGrid = posInGrid.ToDictionary(w => w.Key, w => w.Value);
+                    var workingGrid = grid.ToDictionary(w => w.Key, w => w.Value);
 
-                        var workingPosInGrid = posInGrid.ToDictionary(w => w.Key, w => w.Value);
-                        var workingGrid = grid.ToDictionary(w => w.Key, w => w.Value);
+                    var tile = tiles.First(w => w.Id == 2311);
+                    var placesToPut = new HashSet<(ImageTile tile, int x, int y, int config, int rotation)>();
+                    var additions = FindOrientationsForTile(tile, workingGrid, placesToPut, new[] { (0, 1)});
 
-                        //var placesToPut = new HashSet<(ImageTile tile, int x, int y, int config, int rotation)>();
-                        //var tile = tiles.First(w => w.Id == 2311);
-                        //int additions = FindOrientationsForTile(tile, posInGrid, grid, placesToPut, new[] { (0, 1) });
-
-                        var nodes = new List<(int id, int x, int y, int config, int rotation)>();
-                        if (AttemptFillGrid(workingPosInGrid, workingGrid, neighbourhood, nodes))
-                        {
-                            return workingGrid;
-                        }
-                    }
+                    //var nodes = new List<(int id, int x, int y, int config, int rotation)>();
+                    //if (AttemptFillGrid(workingPosInGrid, workingGrid, neighbourhood, nodes, IsValidLocation))
+                    //{
+                    //    return workingGrid;
+                    //}
                 }
             }
 
             return null;
+
+            bool IsValidLocation((int, int) value)
+            {
+                (int x, int y) = value;
+                if (x < 0 || x > sideLength - 1) return false;
+                if (y < 0 || y > sideLength - 1) return false;
+
+                return true;
+            }
         }
 
-        private static bool AttemptFillGrid(Dictionary<ImageTile, (int x, int y)> posInGrid, Dictionary<(int x, int y), ImageTile> grid, Dictionary<ImageTile, IList<ImageTile>> neighbourhood, IList<(int id, int x, int y, int config, int rotation)> alreadyVisited)
+        private static bool AttemptFillGrid(Dictionary<ImageTile, (int x, int y)> posInGrid, Dictionary<(int x, int y), ImageTile> grid, Dictionary<ImageTile, IList<ImageTile>> neighbourhood, IList<(int id, int x, int y, int config, int rotation)> alreadyVisited, Func<(int, int), bool> locationValidator)
         {
             var placesToPut = new HashSet<(ImageTile tile, int x, int y, int config, int rotation)>();
 
@@ -162,25 +143,6 @@ namespace Day20_ImageTiles
                 .ToHashSet()
                 .Where(w => !posInGrid.ContainsKey(w))
                 .ToList();
-
-            bool IsValidLocation((int, int) value)
-            {
-                (int x, int y) = value;
-                if (x < 0 || x > 3) return false;
-                if (y < 0 || y > 3) return false;
-
-                return true;
-            }
-
-            IEnumerable<(int x, int y)> GetNeighbouringLocations((int, int) value)
-            {
-                (int x, int y) = value;
-
-                yield return (x - 1, y);
-                yield return (x + 1, y);
-                yield return (x, y - 1);
-                yield return (x, y + 1);
-            }
 
             if (tilesToPlace.Count == 0)
                 return true;
@@ -193,17 +155,11 @@ namespace Day20_ImageTiles
                     .Where(w => posInGrid.ContainsKey(w))
                     .Select(w => posInGrid[w])
                     .SelectMany(w => GetNeighbouringLocations(w))
-                    .Where(IsValidLocation)
+                    .Where(locationValidator)
                     .ToHashSet()
                     .ToList();
 
-                int additions = FindOrientationsForTile(tile, posInGrid, grid, placesToPut, gapsToFill);
-
-                //if (additions == 0)
-                //{
-                //    // we were unable to place a neighour. This is not the right branch.
-                //    return false;
-                //}
+                int additions = FindOrientationsForTile(tile, grid, placesToPut, gapsToFill);
             }
 
             foreach (var solution in placesToPut)
@@ -225,7 +181,7 @@ namespace Day20_ImageTiles
                 solution.tile.SetRotateSteps(solution.rotation);
                 solution.tile.Freeze();
 
-                if (AttemptFillGrid(workingPosInGrid, workingGrid, neighbourhood, alreadyVisited))
+                if (AttemptFillGrid(workingPosInGrid, workingGrid, neighbourhood, alreadyVisited, locationValidator))
                 {
                     foreach (var key in workingPosInGrid.Keys)
                     {
@@ -248,13 +204,23 @@ namespace Day20_ImageTiles
             }
 
             return false;
+
+            static IEnumerable<(int x, int y)> GetNeighbouringLocations((int, int) value)
+            {
+                (int x, int y) = value;
+
+                yield return (x - 1, y);
+                yield return (x + 1, y);
+                yield return (x, y - 1);
+                yield return (x, y + 1);
+            }
         }
 
-        private static int FindOrientationsForTile(ImageTile tile, Dictionary<ImageTile, (int x, int y)> posInGrid, Dictionary<(int x, int y), ImageTile> grid, HashSet<(ImageTile tile, int x, int y, int config, int rotation)> placesToPut, IList<(int x, int y)> tilesToFill)
+        private static int FindOrientationsForTile(ImageTile tile, Dictionary<(int x, int y), ImageTile> grid, HashSet<(ImageTile tile, int x, int y, int config, int rotation)> placesToPut, IList<(int x, int y)> tilesToFill)
         {
             int addition = 0;
 
-            var target = posInGrid.Keys.First();
+            var target = grid.Values.First();
 
             foreach ((int x, int y) in tilesToFill)
             {
@@ -306,198 +272,6 @@ namespace Day20_ImageTiles
             }
 
             return addition;
-        }
-
-        class ImageTile
-        {
-            public int Id { get; private set;  }
-
-            public long TopLine => this.cachedTopLine.Value;
-            public long BottomLine => this.cachedBottomLine.Value;
-            public long LeftLine => this.cachedLeftLine.Value;
-            public long RightLine => this.cachedRightLine.Value;
-
-            private readonly List<string> rows = new List<string>();
-
-            private readonly Cached<long> cachedLeftLine;
-            private readonly Cached<long> cachedRightLine;
-            private readonly Cached<long> cachedBottomLine;
-            private readonly Cached<long> cachedTopLine;
-
-            private int rotation = 0;
-            private bool isFlippedHorizontally = false;
-            private bool isFlippedVerticallly = false;
-            private bool isFrozen = false;
-
-            public ImageTile()
-            {
-                this.cachedLeftLine = new Cached<long>(() => this.CalculateLine(Edge.Left));
-                this.cachedRightLine = new Cached<long>(() => this.CalculateLine(Edge.Right));
-                this.cachedBottomLine = new Cached<long>(() => this.CalculateLine(Edge.Bottom));
-                this.cachedTopLine = new Cached<long>(() => this.CalculateLine(Edge.Top));
-            }
-
-            public void AddRow(string row)
-            {
-                if (row.StartsWith("Tile"))
-                {
-                    this.SetId(row);
-                }
-                else
-                {
-                    this.rows.Add(row);
-                    this.ResetLines();
-                }
-            }
-
-            public void Freeze()
-            {
-                this.isFrozen = true;
-            }
-
-            public void UnFreeze()
-            {
-                this.isFrozen = false;
-            }
-
-            public void Rotate90()
-            {
-                if (this.isFrozen) throw new Exception();
-
-                this.rotation++;
-                this.ResetLines();
-            }
-
-            public void SetRotateSteps(int steps)
-            {
-                if (this.isFrozen) throw new Exception();
-
-                this.rotation = steps;
-                this.ResetLines();
-            }
-
-            public int Rotation => this.rotation;
-
-            public void Reset()
-            {
-                if (this.isFrozen) throw new Exception();
-
-                if (this.isFlippedHorizontally)
-                {
-                    this.FlipHorizontal();
-                }
-
-                if (this.isFlippedVerticallly)
-                {
-                    this.FlipVertical();
-                }
-
-                this.rotation = 0;
-                this.ResetLines();
-            }
-
-            public void SetIntoConfig(int config)
-            {
-                if (this.isFrozen) throw new Exception();
-                this.Reset();
-
-                if (config == 1)
-                {
-                    this.FlipVertical();
-                }
-                else if (config == 2)
-                {
-                    this.FlipHorizontal();
-                }
-                else if (config == 3)
-                {
-                    this.FlipVertical();
-                    this.FlipHorizontal();
-                }
-            }
-
-            public override string ToString() => this.Id.ToString();
-
-            private void FlipHorizontal()
-            {
-                if (this.isFrozen) throw new Exception();
-
-                var flippedRows = new List<string>();
-
-                for (int i = 0; i < this.rows.Count; i++)
-                {
-                    flippedRows.Add(new string(this.rows[i].Reverse().ToArray()));
-                }
-
-                this.rows.Clear();
-                this.rows.AddRange(flippedRows);
-
-                this.ResetLines();
-                this.isFlippedHorizontally = !this.isFlippedHorizontally;
-            }
-
-            private void FlipVertical()
-            {
-                this.rows.Reverse();
-                this.ResetLines();
-                this.isFlippedVerticallly = !this.isFlippedVerticallly;
-            }
-
-            private void SetId(string row)
-            {
-                var idString = row[(row.IndexOf(" ") + 1)..^1];
-                this.Id = int.Parse(idString);
-            }
-
-            enum Edge { Left = 1, Right = 2, Bottom = 3, Top = 4 }
-            private long CalculateLine(Edge edge)
-            {
-                string edgeStr = GetEdgeString(edge);
-                long value = 0;
-
-                for (int i = 0; i < edgeStr.Length; i++)
-                {
-                    if (edgeStr[i] == '#')
-                    {
-                        value += (long)Math.Pow(2, i);
-                    }
-                }
-
-                return value;
-            }
-
-            private string GetEdgeString(Edge edge)
-            {
-                var edgeAfterTotation = this.rotation + edge;
-                while ((int)edgeAfterTotation > 4) edgeAfterTotation -= 4;
-                while ((int)edgeAfterTotation <= 0) edgeAfterTotation += 4;
-
-                if (edgeAfterTotation == Edge.Top)
-                {
-                    return this.rows[0];
-                }
-                else if (edgeAfterTotation == Edge.Bottom)
-                {
-                    return this.rows.Last();
-                }
-                else if (edgeAfterTotation == Edge.Left)
-                {
-                    return new string(this.rows.Select(w => w[0]).ToArray());
-                }
-                else if (edgeAfterTotation == Edge.Right)
-                {
-                    return new string(this.rows.Select(w => w[^1]).ToArray());
-                }
-                else throw new Exception();
-            }
-
-            private void ResetLines()
-            {
-                this.cachedLeftLine.Reset();
-                this.cachedRightLine.Reset();
-                this.cachedBottomLine.Reset();
-                this.cachedTopLine.Reset();
-            }
         }
     }
 }
