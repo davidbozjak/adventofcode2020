ed63f93532779bd7fdccdf9da0dd95b65e1e4139
David Božjak <bozjak@gmail.com>
2020-12-25 07:09:24 +0100

    Added day 25

---
+++ Day25_Encryption/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Day25_Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test data
+            //var doorPublicKey = 17807724;
+            //var cardPublicKey = 5764801;
+
+            //real data
+            var doorPublicKey = 8252394;
+            var cardPublicKey = 6269621;
+
+            int doorLoopSize;
+
+            long value = 1;
+            for (doorLoopSize = 0; value != doorPublicKey; doorLoopSize++)
+            {
+                value = EncryptionStep(value, 7);
+            }
+
+            Console.WriteLine($"Door loop size: {doorLoopSize}");
+
+            long encryptionKey = 1;
+
+            for (int i = 0; i < doorLoopSize; i++)
+            {
+                encryptionKey = EncryptionStep(encryptionKey, cardPublicKey);
+            }
+
+            Console.WriteLine($"Part 1: Encryption key: {encryptionKey}");
+        }
+
+        static long EncryptionStep(long value, long subjectMatter)
+        {
+            value = value * subjectMatter;
+            value = value % 20201227;
+
+            return value;
+        }
+    }
+}
